28796be2f6576b2fd731963a46dd5269004931aa
tomf87 <tom.frankland@defra.gov.uk>
2020-03-03 09:32:28 +0000

    minor copy changes
    Fixed typo, changed confirm to consent

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,7 @@ router.get('/adviser/*', function(req, res, next){
 })
 
 router.get('/tier1/*', function(req, res, next){
-  res.locals['serviceName'] = 'Apply for Environmental Land Managment'
+  res.locals['serviceName'] = 'Apply for Environmental Land Management'
 
   next()
 })
