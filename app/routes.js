a12c88d90049d49f00278f89fab53468a8af10d0
tomf87 <tom.frankland@defra.gov.uk>
2020-03-12 16:10:16 +0000

    tier1 v2 prototype
    Duplicated prototype, changed index page.

--- app/routes.js
+++ app/routes.js
@@ -16,37 +16,37 @@ router.get('/adviser/*', function(req, res, next){
   next()
 })
 
-router.get('/tier1/*', function(req, res, next){
+router.get('/tier1-v1/*', function(req, res, next){
   res.locals['serviceName'] = 'Apply for Environmental Land Management'
 
   next()
 })
 
-// Tier 1
+// Tier 1 version 2
 
 // Select an action
 router.post('/actions-answer', function (req, res) {
 
   var action = req.session.data['what-action']
 
-  if (action == "agroforestry"){res.redirect('/tier1/agroforestry')}
-  if (action == "bare-soil"){res.redirect('/tier1/bare-soil')}
-  if (action == "bog-grazing"){res.redirect('/tier1/bog-grazing')}
-  if (action == "canopy-cover"){res.redirect('/tier1/canopy-cover')}
-  if (action == "cover-crops"){res.redirect('/tier1/cover-crops')}
-  if (action == "create-grassland"){res.redirect('/tier1/create-grassland')}
-  if (action == "deadwood"){res.redirect('/tier1/deadwood')}
-  if (action == "drinking-water"){res.redirect('/tier1/drinking-water')}
-  if (action == "fencing"){res.redirect('/tier1/fencing')}
-  if (action == "fertilisers"){res.redirect('/tier1/fertilisers')}
-  if (action == "fertiliser-spreading"){res.redirect('/tier1/fertiliser-spreading')}
-  if (action == "flower-habitat"){res.redirect('/tier1/flower-habitat')}
-  if (action == "grass-strips"){res.redirect('/tier1/grass-strips')}
-  if (action == "manure"){res.redirect('/tier1/manure')}
-  if (action == "no-till"){res.redirect('/tier1/no-till')}
-  if (action == "nutrients"){res.redirect('/tier1/nutrients')}
-  if (action == "tree-belts"){res.redirect('/tier1/tree-belts')}
-  else {res.redirect('/tier1/wind-erosion')}
+  if (action == "agroforestry"){res.redirect('/tier1-v2/agroforestry')}
+  if (action == "bare-soil"){res.redirect('/tier1-v2/bare-soil')}
+  if (action == "bog-grazing"){res.redirect('/tier1-v2/bog-grazing')}
+  if (action == "canopy-cover"){res.redirect('/tier1-v2/canopy-cover')}
+  if (action == "cover-crops"){res.redirect('/tier1-v2/cover-crops')}
+  if (action == "create-grassland"){res.redirect('/tier1-v2/create-grassland')}
+  if (action == "deadwood"){res.redirect('/tier1-v2/deadwood')}
+  if (action == "drinking-water"){res.redirect('/tier1-v2/drinking-water')}
+  if (action == "fencing"){res.redirect('/tier1-v2/fencing')}
+  if (action == "fertilisers"){res.redirect('/tier1-v2/fertilisers')}
+  if (action == "fertiliser-spreading"){res.redirect('/tier1-v2/fertiliser-spreading')}
+  if (action == "flower-habitat"){res.redirect('/tier1-v2/flower-habitat')}
+  if (action == "grass-strips"){res.redirect('/tier1-v2/grass-strips')}
+  if (action == "manure"){res.redirect('/tier1-v2/manure')}
+  if (action == "no-till"){res.redirect('/tier1-v2/no-till')}
+  if (action == "nutrients"){res.redirect('/tier1-v2/nutrients')}
+  if (action == "tree-belts"){res.redirect('/tier1-v2/tree-belts')}
+  else {res.redirect('/tier1-v2/wind-erosion')}
 })
 
 // Arrange a site visit
@@ -57,11 +57,11 @@ router.post('/site-visit-required', function (req, res) {
   var agroForestry = req.session.data['agroforestry']['amount']
   var treeBelts = req.session.data['tree-belts']['amount']
 
-  if (canopyCover != 0){res.redirect('/tier1/site-visit')}
-  if (deadWood != 0){res.redirect('/tier1/site-visit')}
-  if (agroForestry != 0){res.redirect('/tier1/site-visit')}
-  if (treeBelts != 0){res.redirect('/tier1/site-visit')}
-  else {res.redirect('/tier1/calculation')}
+  if (canopyCover != 0){res.redirect('/tier1-v2/site-visit')}
+  if (deadWood != 0){res.redirect('/tier1-v2/site-visit')}
+  if (agroForestry != 0){res.redirect('/tier1-v2/site-visit')}
+  if (treeBelts != 0){res.redirect('/tier1-v2/site-visit')}
+  else {res.redirect('/tier1-v2/calculation')}
 })
 
 // Step-by-step
@@ -72,44 +72,69 @@ router.post('/application-complete', function (req, res) {
   var agroForestry = req.session.data['agroforestry']['amount']
   var treeBelts = req.session.data['tree-belts']['amount']
 
-  if (canopyCover != 0){res.redirect('/tier1/completing-your-application')}
-  if (deadWood != 0){res.redirect('/tier1/completing-your-application')}
-  if (agroForestry != 0){res.redirect('/tier1/completing-your-application')}
-  if (treeBelts != 0){res.redirect('/tier1/completing-your-application')}
-  else {res.redirect('/tier1/agreement-confirmation')}
+  if (canopyCover != 0){res.redirect('/tier1-v2/completing-your-application')}
+  if (deadWood != 0){res.redirect('/tier1-v2/completing-your-application')}
+  if (agroForestry != 0){res.redirect('/tier1-v2/completing-your-application')}
+  if (treeBelts != 0){res.redirect('/tier1-v2/completing-your-application')}
+  else {res.redirect('/tier1-v2/agreement-confirmation')}
 })
 
-/*
+// Tier 1 version 1
 
-// Add an additional farm type
-router.post('/another-type-answer', function (req, res) {
+// Select an action
+router.post('/actions-answer', function (req, res) {
 
-  var anotherType = req.session.data['another-type']
+  var action = req.session.data['what-action']
 
-  if (anotherType == "yes"){
-    res.redirect('/tier1/farm-type-2')
-  }
-  else {
-    res.redirect('/tier1/farm-type-answers')
-  }
-
+  if (action == "agroforestry"){res.redirect('/tier1-v1/agroforestry')}
+  if (action == "bare-soil"){res.redirect('/tier1-v1/bare-soil')}
+  if (action == "bog-grazing"){res.redirect('/tier1-v1/bog-grazing')}
+  if (action == "canopy-cover"){res.redirect('/tier1-v1/canopy-cover')}
+  if (action == "cover-crops"){res.redirect('/tier1-v1/cover-crops')}
+  if (action == "create-grassland"){res.redirect('/tier1-v1/create-grassland')}
+  if (action == "deadwood"){res.redirect('/tier1-v1/deadwood')}
+  if (action == "drinking-water"){res.redirect('/tier1-v1/drinking-water')}
+  if (action == "fencing"){res.redirect('/tier1-v1/fencing')}
+  if (action == "fertilisers"){res.redirect('/tier1-v1/fertilisers')}
+  if (action == "fertiliser-spreading"){res.redirect('/tier1-v1/fertiliser-spreading')}
+  if (action == "flower-habitat"){res.redirect('/tier1-v1/flower-habitat')}
+  if (action == "grass-strips"){res.redirect('/tier1-v1/grass-strips')}
+  if (action == "manure"){res.redirect('/tier1-v1/manure')}
+  if (action == "no-till"){res.redirect('/tier1-v1/no-till')}
+  if (action == "nutrients"){res.redirect('/tier1-v1/nutrients')}
+  if (action == "tree-belts"){res.redirect('/tier1-v1/tree-belts')}
+  else {res.redirect('/tier1-v1/wind-erosion')}
 })
 
-// Add an additional action
-router.post('/another-action-answer', function (req, res) {
+// Arrange a site visit
+router.post('/site-visit-required', function (req, res) {
 
-  var anotherType = req.session.data['another-action']
+  var canopyCover = req.session.data['canopy-cover']['amount']
+  var deadWood = req.session.data['deadwood']['amount']
+  var agroForestry = req.session.data['agroforestry']['amount']
+  var treeBelts = req.session.data['tree-belts']['amount']
 
-  if (anotherType == "yes"){
-    res.redirect('/tier1/actions')
-  }
-  else {
-    res.redirect('/tier1/actions-answers')
-  }
-
+  if (canopyCover != 0){res.redirect('/tier1-v1/site-visit')}
+  if (deadWood != 0){res.redirect('/tier1-v1/site-visit')}
+  if (agroForestry != 0){res.redirect('/tier1-v1/site-visit')}
+  if (treeBelts != 0){res.redirect('/tier1-v1/site-visit')}
+  else {res.redirect('/tier1-v1/calculation')}
 })
 
-*/
+// Step-by-step
+router.post('/application-complete', function (req, res) {
+
+  var canopyCover = req.session.data['canopy-cover']['amount']
+  var deadWood = req.session.data['deadwood']['amount']
+  var agroForestry = req.session.data['agroforestry']['amount']
+  var treeBelts = req.session.data['tree-belts']['amount']
+
+  if (canopyCover != 0){res.redirect('/tier1-v1/completing-your-application')}
+  if (deadWood != 0){res.redirect('/tier1-v1/completing-your-application')}
+  if (agroForestry != 0){res.redirect('/tier1-v1/completing-your-application')}
+  if (treeBelts != 0){res.redirect('/tier1-v1/completing-your-application')}
+  else {res.redirect('/tier1-v1/agreement-confirmation')}
+})
 
 // Land managers
 
