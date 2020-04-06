e0cedd2f397618dafae4e4dd087af1dfc84bfb5c
Ben Wakefield <ben@eyedc.co>
2020-04-06 11:46:18 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Environmental land management',
+  serviceName: 'Apply for ELM',
 
   // Default port that prototype runs on
   port: '3000',
