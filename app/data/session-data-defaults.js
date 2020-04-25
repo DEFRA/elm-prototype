8684462cd6f3f3c1766c103156d954d95cec86ae
Ben Wakefield <ben@eyedc.co>
2020-04-25 22:50:45 +0100

    Uplands and arable policy standards
    plus agreement update

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -40,10 +40,10 @@ module.exports = {
   "postcode": "DL10 4AT",
 
   "businessemail": "johnsmith1950.gmail.com",
-  "businesaddress1": "Willow Farm",
-  "businesaddress2": "Richmond",
-  "businesaddress3": "North Yorks",
-  "businespostcode": "DL10 4AT",
+  "businessaddress1": "Willow Farm",
+  "businessaddress2": "Richmond",
+  "businessaddress3": "North Yorks",
+  "businesspostcode": "DL10 4AT",
 
   "holding-size": 130,
   
